41ee8e75abb58e36add346769c5351440a25e9a9
Dymas <dymas4464@gmail.com>
2025-09-10 17:11:09 +0700

    Day1 - JavaScript

---
+++ JavaScript/Script/day1-script.js
@@ -0,0 +1,15 @@
+// Day 1 - Variable - Output
+
+// Deklarasi variable
+let nama = "Dymas";
+const umur = 22;
+var hobi = "Programming";
+
+// Ooutput ke Console
+console.log("Halo Nama Saya " + nama);
+console.log("Umur Saya " + umur + " Tahun");
+console.log("Hobi Saya Adalah " + hobi);
+
+// Output ke Halaman HTML
+document.getElementById("output").innerHTML =
+"Halo, Saya " + nama + ". Umur saya " + umur + ". Hobi Saya adalah " + hobi + ".";
